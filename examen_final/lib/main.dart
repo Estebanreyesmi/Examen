0a4c9b7b32225070356782bc40dbe13ec8700365
Estebanreyesmi <esteban.reyes@mayor.cl>
2024-05-11 18:13:19 -0400

    v1

--- examen_final/lib/main.dart
+++ examen_final/lib/main.dart
@@ -1,7 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:examen_final/routes/app_routes.dart';
+import 'package:examen_final/services/auth_service.dart';
+import 'package:examen_final/services/product_service.dart';
+import 'package:examen_final/theme/my_theme.dart';
+import 'package:provider/provider.dart';
 
-void main() {
-  runApp(const MainApp());
+void main() => runApp(AppState());
+
+class AppState extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => AuthService()),
+        ChangeNotifierProvider(create: (_) => ProductService()),
+      ],
+      child: const MainApp(),
+    );
+  }
 }
 
 class MainApp extends StatelessWidget {
@@ -9,12 +25,13 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Material App',
+      initialRoute: AppRoutes.initialRoute,
+      routes: AppRoutes.routes,
+      onGenerateRoute: AppRoutes.onGenerateRoute,
+      theme: MyTheme.myTheme,
     );
   }
 }
---
+++ examen_final/lib/providers/providers.dart
@@ -0,0 +1,2 @@
+export 'package:examen_final/providers/login_form_provider.dart';
+export 'package:examen_final/providers/product_form_provider.dart';
